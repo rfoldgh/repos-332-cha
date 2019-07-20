ae184dd707ae4795b6c244612877c1b3aa6a5587
rfoldgh <foldenauerr@sfusd.edu>
2019-07-19 21:10:48 -0700

    Add files via upload

--- models/item.js
+++ models/item.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true }
+ //   description: { type: String, required: true }
 });
 
 var Item = mongoose.model('Item', ItemSchema);
